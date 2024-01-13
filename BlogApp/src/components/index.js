96d0f889cb62302e443723ec5624925455d04bd9
AMOGHPROMODPHATARPEKAR <amoghpp129@gmail.com>
2024-01-13 22:03:22 +0530

    Components done

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -1,7 +1,24 @@
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Input from "./Input";
+import Protected from "./AuthLayout";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    Button,
+    PostCard,
+    Select,
+    Input,
+    Protected,
+    LogoutBtn
+
 }
